0246047901b1b9d0a5d0a6d396bec18d01db8670
Urbanek, David <david.urbanek@aruplab.com>
2024-10-08 08:10:16 -0600

    Add project files.

---
+++ WordleModels/GameMove.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace WordleModels;
+
+public class GameMove
+{
+    [JsonProperty(PropertyName = "gameId")]
+    public string GameId { get; set; } = string.Empty;
+
+    [JsonProperty(PropertyName = "moveNumber")]
+    public int MoveNumber { get; set; }
+
+    [JsonProperty(PropertyName = "ruledOut")]
+    public List<string> RuledOut { get; set; } = new List<string>();
+
+    [JsonProperty(PropertyName = "correct")]
+    public List<string> Correct { get; set; } = new List<string>();
+
+    [JsonProperty(PropertyName = "incorrect")]
+    public List<List<string>> Incorrect { get; set; } = new List<List<string>>();
+
+
+}
